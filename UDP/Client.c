6f7dec3dd2a38b81dc9629f75afc233c3b3c0074
AkshayShaji <95688691+AkshayShaji@users.noreply.github.com>
2023-08-03 14:52:28 +0530

    Update Client.c

--- UDP/Client.c
+++ UDP/Client.c
@@ -1,4 +1,6 @@
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <stdio.h>
@@ -8,29 +10,33 @@
 int main(int argc,char* argv[])
 {
   int k;
-  int sockfd;
-  struct sockaddr_in server,client;
+  int sock_desc;
+  struct sockaddr_in server;
   char buf[100];
-  if(argc!=3)
+  socklen_t len;
+  if(argc!=2)
   {
     printf("Input format is not correct\n");
-    return 1;
+    exit(0);
   }
-  sockfd=socket(AF_INET,SOCK_STREAM,0);
-  if(sockfd==-1)
+  sock_desc=socket(AF_INET,SOCK_DGRAM,0);
+  if(sock_desc==-1)
   {
     printf("Error in creation of socket\n");
-    return 1;
+    return -1;
   }
   server.sin_family=AF_INET;
   server.sin_addr.s_addr=INADDR_ANY;
   server.sin_port=htons(atoi(argv[2]));
   printf("Enter the message to be send: ");
   fgets(buf,100,stdin);
-  k=sendto(sockfd,buf,100,0,(struct sockaddr*)&server,sizeof(server));
-  if(k<0)
+  len=sizeof(server);
+  k=sendto(sock_desc,buf,100,0,(struct sockaddr*)&server,len);
+  if(k==-1)
   {
     printf("Error in sending message to server\n");
-    return 1;
+    return -1;
   }
+  close(sock_desc);
+  return 0;
 }
